1e938d31fda3215c9db062dc67dce08205a28acd
Nico Pom <niconp7@yahoo.fr>
2022-04-11 20:15:59 +0200

    updating carousel / lightbox / active states
    desktop carousel update :
    - active states
    - lightbox
    - switching photos by clicking miniature

--- main.js
+++ main.js
@@ -1,65 +1,110 @@
 // SIDE BAR MENU
-const btnClose = document.getElementById("btn-close")
-const btnMenu = document.getElementById("btn-menu")
-const overlay = document.getElementById("overlay")
-const sideNav = document.getElementById("side-nav")
+const btnClose = document.getElementById("btn-close");
+const btnMenu = document.getElementById("btn-menu");
+const overlay = document.getElementById("overlay");
+const sideNav = document.getElementById("side-nav");
 
-btnMenu.addEventListener("click", ()=>{
+btnMenu.addEventListener("click", () => {
     overlay.style.transform = "translateX(0%)";
     setTimeout(() => {
         sideNav.style.transform = "translateX(0%)";
     }, 50);
-})
-btnClose.addEventListener("click", ()=>{
+});
+btnClose.addEventListener("click", () => {
     sideNav.style.transform = "translateX(-100%)";
     setTimeout(() => {
         overlay.style.transform = "translateX(-100%)";
     }, 300);
-})
+});
 
 // CAROUSEL
-const slides = document.getElementsByClassName('carousel-item');
+
+let slides = document.querySelectorAll(".carousel .carousel-item");
 let slidePosition = 0;
 const totalSlides = slides.length;
 
-document.getElementById('carousel-button-next').addEventListener('click', moveToNextSlide);
-document.getElementById('carousel-button-prev').addEventListener('click', moveToPrevSlide);
+document
+    .getElementById("carousel-button-next")
+    .addEventListener("click", moveToNextSlide);
+document
+    .getElementById("carousel-button-prev")
+    .addEventListener("click", moveToPrevSlide);
 
 function hideAllSlides() {
     for (let slide of slides) {
-        slide.classList.remove('carousel-item-visible');
-        slide.classList.add('carousel-item-hidden');
+        slide.classList.remove("carousel-item-visible");
+        slide.classList.add("carousel-item-hidden");
     }
 }
 
 function moveToNextSlide() {
     hideAllSlides();
-    
+
     if (slidePosition === totalSlides - 1) {
         slidePosition = 0;
     } else {
         slidePosition++;
     }
-    
+
     slides[slidePosition].classList.add("carousel-item-visible");
 }
 
 function moveToPrevSlide() {
     hideAllSlides();
-    
+
     if (slidePosition === 0) {
         slidePosition = totalSlides - 1;
     } else {
         slidePosition--;
     }
-    
+
     slides[slidePosition].classList.add("carousel-item-visible");
 }
 
-// BASKET
-const btnCart = document.getElementById("btn-cart")
-const basket = document.getElementById("basket")
+//CAROUSEL LIST
+const carouselListEl = document.querySelectorAll(".carousel-list-el");
 
-btnCart.addEventListener("click", ()=>{
-    basket.classList.toggle("visible")
-})
+carouselListEl.forEach((el) => {
+    el.addEventListener("click", () => {
+        hideAllSlides();
+        slides[el.id].classList.add("carousel-item-visible");
+        removeSelector();
+        el.classList.add("selected");
+    });
+});
+
+function removeSelector() {
+    for (let el of carouselListEl) {
+        el.classList.remove("selected");
+    }
+}
+
+//LIGHTBOX
+const btnCloseLightbox = document.getElementById("btn-close-lightbox");
+const overlayLightbox = document.getElementById("overlay-lightbox");
+
+//open and close lightbox
+if (window.innerWidth >= 900) {
+    slides.forEach((slide) => {
+        slide.addEventListener("click", () => {
+            overlayLightbox.style.display = "grid";
+            slides = document.querySelectorAll(
+                ".carousel-lightbox .carousel-item"
+            );
+        });
+    });
+
+    btnCloseLightbox.addEventListener("click", () => {
+        overlayLightbox.style.display = "none";
+        slides = document.querySelectorAll(".carousel .carousel-item");
+    });
+}
+//lightbox carousel
+
+// BASKET
+const btnCart = document.getElementById("btn-cart");
+const basket = document.getElementById("basket");
+
+btnCart.addEventListener("click", () => {
+    basket.classList.toggle("visible");
+});
